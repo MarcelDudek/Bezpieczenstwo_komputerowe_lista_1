4fd92421be8af41078f7f9d48cba0799e6141c4a
Marcel Dudek <lotnik1998@gmail.com>
2020-11-04 23:41:01 +0100

    Added Cracker class.
    Currently with load_from_files method for loading multiple cryptograms.

--- zadanie1.py
+++ zadanie1.py
@@ -1,7 +1,8 @@
 import binascii
 
 class Cryptogram:
-    message = bytearray()
+    def __init__(self):
+        self.message = bytearray()
 
     def load(self, path):
         f = open(path)
@@ -16,12 +17,32 @@ class Cryptogram:
                     byte += 1 << shift
                 shift -= 1
             self.message.append(byte)
+        f.close()
 
     def print(self):
         print(binascii.hexlify(self.message))
 
 
+class Cracker:
+    def __init__(self):
+        self.cryptograms = []
 
-c = Cryptogram()
-c.load('test.txt')
-c.print()
+    def load_from_files(self, paths):
+        for path in paths:
+            c = Cryptogram()
+            c.load(path)
+            self.cryptograms.append(c)
+
+    def print_all(self):
+        for c in self.cryptograms:
+            c.print()
+
+
+c = Cracker()
+paths = ['cryptograms/1.txt', 'cryptograms/2.txt', 'cryptograms/3.txt', 'cryptograms/4.txt',
+'cryptograms/5.txt', 'cryptograms/6.txt', 'cryptograms/7.txt', 'cryptograms/8.txt',
+'cryptograms/9.txt', 'cryptograms/10.txt', 'cryptograms/11.txt', 'cryptograms/12.txt', 
+'cryptograms/13.txt', 'cryptograms/14.txt', 'cryptograms/15.txt', 'cryptograms/16.txt', 
+'cryptograms/17.txt', 'cryptograms/18.txt', 'cryptograms/19.txt', 'cryptograms/20.txt']
+c.load_from_files(paths)
+c.print_all()
