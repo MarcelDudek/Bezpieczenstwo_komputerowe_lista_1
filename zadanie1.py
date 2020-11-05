0f53cf80396b426eec3f85bb3a0e675122dc5fd8
Marcel Dudek <lotnik1998@gmail.com>
2020-11-05 02:17:38 +0100

    First cracking of the key
    - added cracking functions
    - key is being cracked, but not tested yet if it is correct
    - added some printing functions
    - added some overriten functions for Cryptogram class

--- zadanie1.py
+++ zadanie1.py
@@ -21,11 +21,19 @@ class Cryptogram:
 
     def print(self):
         print(binascii.hexlify(self.message))
+    
+    def __len__(self):
+        return len(self.message)
+
+    def __getitem__(self, i):
+        return self.message[i]
 
 
 class Cracker:
     def __init__(self):
         self.cryptograms = []
+        self.key = bytearray()
+        self.key_cracked = bytearray()
 
     def load_from_files(self, paths):
         for path in paths:
@@ -37,6 +45,53 @@ class Cracker:
         for c in self.cryptograms:
             c.print()
 
+    def crack(self):
+        self.key = bytearray()
+        self.key_cracked = bytearray()
+
+        # find out the max length of the key
+        max_len = 0
+        for c in self.cryptograms:
+            if len(c) > max_len:
+                max_len = len(c)
+        self.key = bytearray(max_len)
+        self.key_cracked = bytearray(max_len)
+        for i in range(len(self.cryptograms)):
+            for j in range(i, len(self.cryptograms)):
+                for k in range(j, len(self.cryptograms)):
+                    self.__crack_three(self.cryptograms[i], self.cryptograms[j], self.cryptograms[k])
+
+    def __crack_three(self, c1: Cryptogram, c2: Cryptogram, c3: Cryptogram):
+        l = min([len(c1), len(c2), len(c3)])
+        for i in range(l):
+            if not self.key_cracked[i]:
+                k = self.__get_key(c1[i], c2[i], c3[i])
+                if k is not None:
+                    self.key[i] = k
+                    self.key_cracked[i] = 0xFF
+
+    def __get_key(self, c1: int, c2: int, c3: int):
+        space = 0b01000000
+        c1c2 = c1 ^ c2
+        c1c3 = c1 ^ c3
+        c2c3 = c2 ^ c3
+        if c1c2 & space and c1c3 & space and c2c3 & space:  # impossible to distinguish
+            return None
+        elif c1c2 & space and c1c3 & space:  # c1 is space
+            return c1 ^ space
+        elif c1c2 & space and c2c3 & space:  # c2 is space
+            return c2 ^ space
+        elif c1c3 & space and c2c3 & space:  # c3 is space
+            return c3 ^ space
+        else:
+            return None
+
+    def print_key(self):
+        print(binascii.hexlify(self.key))
+
+    def print_cracked_key_bytes(self):
+        print(binascii.hexlify(self.key_cracked))
+
 
 c = Cracker()
 paths = ['cryptograms/1.txt', 'cryptograms/2.txt', 'cryptograms/3.txt', 'cryptograms/4.txt',
@@ -45,4 +100,7 @@ paths = ['cryptograms/1.txt', 'cryptograms/2.txt', 'cryptograms/3.txt', 'cryptog
 'cryptograms/13.txt', 'cryptograms/14.txt', 'cryptograms/15.txt', 'cryptograms/16.txt', 
 'cryptograms/17.txt', 'cryptograms/18.txt', 'cryptograms/19.txt', 'cryptograms/20.txt']
 c.load_from_files(paths)
-c.print_all()
+# c.print_all()
+c.crack()
+c.print_key()
+c.print_cracked_key_bytes()
