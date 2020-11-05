544bd33036d76cb8e6ac4c88705e07cec321d1a8
Marcel Dudek <lotnik1998@gmail.com>
2020-11-05 02:51:08 +0100

    Fix cracking and added some printing and access methods
    - fixed bug in cracking (wrong space char value)
    - added some printing function
    - added decoded string function getter for Cryptogram class

--- zadanie1.py
+++ zadanie1.py
@@ -28,6 +28,14 @@ class Cryptogram:
     def __getitem__(self, i):
         return self.__message[i]
 
+    def decoded_message(self, key: bytearray, key_cracked: bytearray):
+        decoded = bytes(a ^ b for (a, b) in zip(self.__message, key))
+        text = decoded.decode('ascii', errors='ignore')
+        for i in range(len(text)):
+            if not key_cracked[i]:
+                text = text[:i] + '#' + text[i + 1:]
+        return text
+
 
 class Cracker:
     def __init__(self):
@@ -71,17 +79,18 @@ class Cracker:
                     self.key_cracked[i] = 0xFF
 
     def __get_key(self, c1: int, c2: int, c3: int):
-        space = 0b01000000
+        space_present = 0b01000000
+        space = 0b00100000
         c1c2 = c1 ^ c2
         c1c3 = c1 ^ c3
         c2c3 = c2 ^ c3
-        if c1c2 & space and c1c3 & space and c2c3 & space:  # impossible to distinguish
+        if c1c2 & space_present and c1c3 & space_present and c2c3 & space_present:  # impossible to distinguish
             return None
-        elif c1c2 & space and c1c3 & space:  # c1 is space
+        elif c1c2 & space_present and c1c3 & space_present:  # c1 is space
             return c1 ^ space
-        elif c1c2 & space and c2c3 & space:  # c2 is space
+        elif c1c2 & space_present and c2c3 & space_present:  # c2 is space
             return c2 ^ space
-        elif c1c3 & space and c2c3 & space:  # c3 is space
+        elif c1c3 & space_present and c2c3 & space_present:  # c3 is space
             return c3 ^ space
         else:
             return None
@@ -91,6 +100,10 @@ class Cracker:
 
     def print_cracked_key_bytes(self):
         print(binascii.hexlify(self.key_cracked))
+
+    def print_cracked_cryptograms(self):
+        for i, c in enumerate(self.__cryptograms):
+            print('Cryptogram ', i, ':\n', c.decoded_message(self.key, self.key_cracked), '\n')
 
 
 c = Cracker()
@@ -102,5 +115,6 @@ paths = ['cryptograms/1.txt', 'cryptograms/2.txt', 'cryptograms/3.txt', 'cryptog
 c.load_from_files(paths)
 # c.print_all()
 c.crack()
-c.print_key()
-c.print_cracked_key_bytes()
+# c.print_key()
+# c.print_cracked_key_bytes()
+c.print_cracked_cryptograms()
